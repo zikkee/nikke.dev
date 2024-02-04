0848a2dfda5a0418fbf768067852660464ee7bb0
Nikke <62788198+zikkee@users.noreply.github.com>
2024-02-04 12:48:01 +0200

    lisätty lähdekoodilinkki

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const currentYear = new Date().getFullYear();
 
@@ -7,7 +8,7 @@ const Footer = () => {
     <footer>
         <div className="flex flex-col items-center justify-center bg-gray-900 text-white p-5 rounded-t-3xl">
             <p className='text-center'>© {currentYear} NikkeDev</p>
-            <p className='text-center'>All rights reserved.</p>
+            <p className='text-center'>Source code available on <Link href="https://github.com/zikkee/nikke.dev" passHref className='underline'>GitHub</Link></p>
         </div>
     </footer>
   );
