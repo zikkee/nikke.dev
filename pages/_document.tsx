4d4f4617b4c4782c45797ec922fa7437e5ec237e
Nikke <nikke@nikke.dev>
2024-04-07 22:03:47 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,21 @@ class MyDocument extends Document {
             <meta name="description" content="a teen who enjoys system administration and coding" />
             <meta name="keywords" content="nikke, nikke.dev, nikkedev, zikke, zikkee, N1kkee" />
             <meta name="theme-color" content="#000000" />
-            <script defer data-domain="nikke.dev" src="https://analytics.nikke.dev/js/script.js"></script>
+            <!-- Matomo -->
+            <script>
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//analytics.nikke.dev/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            </script>
+            <!-- End Matomo Code -->
         </Head>
         <body>
           <Main />
